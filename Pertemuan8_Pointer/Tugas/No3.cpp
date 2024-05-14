4b93052a55d7f9f6ab7c30e11e1733f9b31f8e73
ALPHARESZ <jeremyalpharesz70@gmail.com>
2024-05-14 12:59:03 +0700

    Tugas 8

--- Pertemuan8_Pointer/Tugas/No3.cpp
+++ Pertemuan8_Pointer/Tugas/No3.cpp
@@ -6,8 +6,8 @@ int main() {
   
   char *letter = "K O M P U T E R";
 
+  cout << letter << endl;
   letter += 8;
-
-  cout << *letter;
+  cout << "Huruf ke-5 dari K O M P U T E R adalah "<<*letter;
   return 0;
 }
